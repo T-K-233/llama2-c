958e59b81c2aaa0e219d16dab7fe61edd24074c7
-T.K.- <t_k_233@outlook.com>
2024-11-25 17:31:47 -0800

    ADD: handy script to download checkpoints

---
+++ download_checkpoints.sh
@@ -0,0 +1,3 @@
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories260K/stories260K.bin
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories260K/tok512.bin
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin
